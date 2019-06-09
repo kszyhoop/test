e78aa32a3683a1bf493899ae0181de3189f3336c
kszyhoop <jamulear@o2.pl>
2019-06-10 00:00:01 +0200

    Update Client_GameRefresh.lua

--- Client_GameRefresh.lua
+++ Client_GameRefresh.lua
@@ -1,6 +1,9 @@
 function Client_GameRefresh(game)
 	if game.Game.TurnNumber > 0 then return end; -- don't pop up in ongoing games, could be removed later
 
+	if (game.Us ~= nil) then
+		UI.Alert(game.Us)
+	end
 	if not Mod.PlayerGameData.InitialPopupDisplayed then
 		UI.Alert("This game includes Swap Picks Mod - you pick for your opponent. Good luck :)")
 		local payload = {};
